2cb9345ed52257c9c2268f289de45fa309787f7b
seanchavez <realseanchavez@gmail.com>
2021-08-18 17:45:00 -0700

    hog prob 10

--- hog/hog.py
+++ hog/hog.py
@@ -358,7 +358,7 @@ def bacon_strategy(score, opponent_score, margin=8, num_rolls=6):
     rolls NUM_ROLLS otherwise.
     """
     # BEGIN PROBLEM 10
-    return 6  # Replace this statement
+    return 0 if free_bacon(opponent_score) >= margin else num_rolls
     # END PROBLEM 10
 
 
